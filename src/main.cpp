33779c32736aa94e9576045655b0c6a0274c00e1
Jiří Šebele <sindriava@gmail.com>
2016-08-29 20:33:17 +0200

    Add font rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <cstdio>
 
 #include <SDL.h>
+#include <SDL_ttf.h>
 
 const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main() {
+  /* All systems go! */
   if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
     printf("Failed to initilize SDL.\nSDL_Error: %s\n", SDL_GetError());
+    return 1;
   }
 
+  if (TTF_Init() < 0) {
+    printf("Failed to initialize SDL_ttf.\nTTF_Error: %s\n", TTF_GetError());
+    return 1;
+  }
+
+  /* Create the window */
   SDL_Window *window = SDL_CreateWindow("Rogue",
     SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
     SCREEN_WIDTH, SCREEN_HEIGHT,
@@ -18,10 +27,36 @@ int main() {
 
   if (window == nullptr) {
     printf("Failed to create window.\nSDL_Error: %s\n", SDL_GetError());
+    return 1;
   }
 
-  SDL_Surface *surface = SDL_GetWindowSurface(window);
+  /* Create the renderer */
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+  if (renderer == nullptr) {
+    printf("Failed to create rederer.\nSDL_Error: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+  /* Set up text rendering */
+  TTF_Font *font = TTF_OpenFont("res/PxPlus_IBM_VGA8.ttf", 28);
+
+  if (font == nullptr) {
+    printf("Failed to load font.\nTTF_Error: %s\n", TTF_GetError());
+    return 1;
+  }
+
+  SDL_Surface *_text = TTF_RenderText_Solid(font, "Hello, world!", {0, 0, 0});
+  SDL_Texture *text = SDL_CreateTextureFromSurface(renderer, _text);
+  SDL_FreeSurface(_text); 
+
+  SDL_Rect text_rect { 100, 100, 0, 0 };
+
+  SDL_QueryTexture(text, nullptr, nullptr, &text_rect.w, &text_rect.h);
+
+  /* Main loop */
   bool quit = false;
   SDL_Event e;
 
@@ -32,12 +67,16 @@ int main() {
       }
     }
 
-    SDL_FillRect(surface, nullptr,
-        SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
-
-    SDL_UpdateWindowSurface(window);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, text, nullptr, &text_rect);
+    SDL_RenderPresent(renderer);
   }
 
+  TTF_CloseFont(font);
+  SDL_DestroyTexture(text);
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
+
+  TTF_Quit();
   SDL_Quit();
 }
