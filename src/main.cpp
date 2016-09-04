e6326a19e52d06d519b564463c03bf4321fcdb60
Jiří Šebele <sindriava@gmail.com>
2016-09-04 12:19:27 +0200

    Add a logger for on-screen text rendering

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,234 @@ using namespace glm;
 const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
 
+struct Character {
+  GLuint textureID; // ID handle of the glyph texture
+  ivec2  size;      // Size of glyph
+  ivec2  bearing;   // Offset from baseline to left/top of glyph
+  GLuint advance;   // Offset to advance to next glyph
+};
+
+class Font {
+  private:
+    std::vector<Character> characters;
+    Shader shader;
+
+    GLuint vao;
+    GLuint vbo;
+
+  public:
+    Font(FT_Library ft, std::string path)
+      : shader("res/text.vert", "res/text.frag")
+    {
+      /* Load the face */
+      FT_Face face;
+
+      if (FT_New_Face(ft, path.c_str(), 0, &face)) {
+        throw std::runtime_error("Failed to load font '" + path + "'.");
+      }
+
+      FT_Set_Pixel_Sizes(face, 0, 16);
+
+      /* Generate the charset */
+      glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+  
+      for (GLubyte c = 0; c < 128; c++) {
+        /* Load character glyph */
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+          fprintf(stderr, "Failed to load glyph 0x%x.\n", c);
+          continue;
+        }
+
+        /* Generate texture */
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+            );
+
+        /* Set texture options */
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        /* Now store character for later use */
+        Character character = {
+          texture, 
+          ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+          ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+          (GLuint) face->glyph->advance.x
+        };
+
+        characters.push_back(character);
+      }
+
+      /* Clean up after FreeType */
+      FT_Done_Face(face);
+
+      /* Prepare vertex arrays */
+      glGenVertexArrays(1, &vao);
+      glGenBuffers(1, &vbo);
+
+      glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid *)(2 * sizeof(GLfloat)));
+        glEnableVertexAttribArray(1);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+      glBindVertexArray(0);  
+    }
+
+    void render(std::string text, vec2 position, vec3 color = vec3(0), float scale = 1.0f) {
+      /* Activate corresponding shader */
+      shader.use();
+
+      shader.setUniform("textColor", color);
+      shader.setUniform("projection", ortho(0.0f, (float)SCREEN_WIDTH, (float)SCREEN_HEIGHT, 0.0f));
+
+      glActiveTexture(GL_TEXTURE0);
+      glBindVertexArray(vao);
+
+      /* Iterate through all characters */
+      for (auto c = text.begin(); c != text.end(); c++) {
+        Character ch = characters[*c];
+
+        GLfloat xpos = position.x + ch.bearing.x * scale;
+        GLfloat ypos = position.y - ch.bearing.y * scale;
+
+        GLfloat w = ch.size.x * scale;
+        GLfloat h = ch.size.y * scale;
+
+        /* Update VBO for each character */
+        GLfloat vertices[24] = {
+          xpos,     ypos + h,   0.0, 1.0,            
+          xpos,     ypos,       0.0, 0.0,
+          xpos + w, ypos,       1.0, 0.0,
+
+          xpos,     ypos + h,   0.0, 1.0,
+          xpos + w, ypos,       1.0, 0.0,
+          xpos + w, ypos + h,   1.0, 1.0,          
+        };
+
+        /* Render glyph texture over quad */
+        glBindTexture(GL_TEXTURE_2D, ch.textureID);
+
+        /* Update content of VBO memory */
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); 
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        /* Render quad */
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        position.x += (ch.advance >> 6) * scale;
+      }
+
+      shader.disuse();
+
+      glBindVertexArray(0);
+      glBindTexture(GL_TEXTURE_2D, 0);
+    }
+};
+
+class LogWindow {
+  private:
+    vec2 position;
+
+    std::vector<std::string> messages;
+    uint32_t messageCount;
+
+    Font & font;
+
+    Shader shader;
+    GLuint vao, vbo;
+    std::vector<GLfloat> vertices;
+
+  public:
+    LogWindow(const vec2 & p, uint32_t mc, Font & f)
+      : position(p)
+      , messageCount(mc)
+      , font(f)
+      , shader("res/ui.vert", "res/ui.frag")
+    {
+
+      vertices.insert(vertices.end(), {
+        p.x - 4,       p.y + 4,
+        p.x - 4 + 480, p.y + 4,
+        p.x - 4,       p.y + 4 - 120,
+
+        p.x - 4 + 480, p.y + 4,
+        p.x - 4,       p.y + 4 - 120,
+        p.x - 4 + 480, p.y + 4 - 120,
+      });
+
+      glGenVertexArrays(1, &vao);
+      glGenBuffers(1, &vbo);
+
+      glBindVertexArray(vao);
+
+      glBindBuffer(GL_ARRAY_BUFFER, vbo);
+      glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
+
+      /* Position attribute */
+      glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (GLvoid*)0);
+      glEnableVertexAttribArray(0);
+
+      glBindVertexArray(0);
+    }
+
+    void render() {
+      if (messages.size() > messageCount) {
+        messages.resize(messageCount);
+      }
+
+      shader.use();
+
+      shader.setUniform("projection", ortho(0.0f, (float)SCREEN_WIDTH, (float) SCREEN_HEIGHT, 0.0f));
+
+      glBindVertexArray(vao);
+      glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+      glBindVertexArray(0);
+
+      shader.disuse();
+
+      for (uint32_t i = 0; i < messages.size(); i++) {
+        font.render(messages[messages.size() - i - 1], vec2(position.x, position.y - i * 12), vec3(1.0 - (1.0 / messageCount) * i), 0.75);
+      }
+    }
+
+    void log(std::string message) {
+      messages.push_back(message);
+    }
+};
+
+class Logger {
+  public:
+    static LogWindow * window;
+
+    static void log(std::string message) {
+      if (window != nullptr) {
+        window->log(message);
+      }
+    }
+};
+
+LogWindow * Logger::window;
+
 class GraphicsContext {
   private:
     Shader & shader;
@@ -159,7 +387,8 @@ class Obelisk : public Entity {
     }
 
     bool interact() override {
-      printf("Stuff is inscribed in the stone in an acient script. You can't read it for shit.\n");
+      Logger::log("Stuff is inscribed in the stone in an acient script");
+      Logger::log("You can't read it for shit");
       return true;
     }
 
@@ -691,150 +920,6 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
   }
 }
 
-struct Character {
-  GLuint textureID; // ID handle of the glyph texture
-  ivec2  size;      // Size of glyph
-  ivec2  bearing;   // Offset from baseline to left/top of glyph
-  GLuint advance;   // Offset to advance to next glyph
-};
-
-class Font {
-  private:
-    std::vector<Character> characters;
-    Shader shader;
-
-    GLuint vao;
-    GLuint vbo;
-
-  public:
-    Font(FT_Library ft, std::string path)
-      : shader("res/text.vert", "res/text.frag")
-    {
-      /* Load the face */
-      FT_Face face;
-
-      if (FT_New_Face(ft, path.c_str(), 0, &face)) {
-        throw std::runtime_error("Failed to load font '" + path + "'.");
-      }
-
-      FT_Set_Pixel_Sizes(face, 0, 16);
-
-      /* Generate the charset */
-      glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-  
-      for (GLubyte c = 0; c < 128; c++) {
-        /* Load character glyph */
-        if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
-          fprintf(stderr, "Failed to load glyph 0x%x.\n", c);
-          continue;
-        }
-
-        /* Generate texture */
-        GLuint texture;
-        glGenTextures(1, &texture);
-        glBindTexture(GL_TEXTURE_2D, texture);
-        glTexImage2D(
-            GL_TEXTURE_2D,
-            0,
-            GL_RED,
-            face->glyph->bitmap.width,
-            face->glyph->bitmap.rows,
-            0,
-            GL_RED,
-            GL_UNSIGNED_BYTE,
-            face->glyph->bitmap.buffer
-            );
-
-        /* Set texture options */
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-        /* Now store character for later use */
-        Character character = {
-          texture, 
-          ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
-          ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
-          (GLuint) face->glyph->advance.x
-        };
-
-        characters.push_back(character);
-      }
-
-      /* Clean up after FreeType */
-      FT_Done_Face(face);
-
-      /* Prepare vertex arrays */
-      glGenVertexArrays(1, &vao);
-      glGenBuffers(1, &vbo);
-
-      glBindVertexArray(vao);
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
-
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
-        glEnableVertexAttribArray(0);
-
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (GLvoid *)(2 * sizeof(GLfloat)));
-        glEnableVertexAttribArray(1);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-      glBindVertexArray(0);  
-    }
-
-    void render(std::string text, vec2 position, vec3 color = vec3(0), float scale = 1.0f) {
-      /* Activate corresponding shader */
-      shader.use();
-
-      shader.setUniform("textColor", color);
-      shader.setUniform("projection", ortho(0.0f, (float)SCREEN_WIDTH, (float)SCREEN_HEIGHT, 0.0f));
-
-      glActiveTexture(GL_TEXTURE0);
-      glBindVertexArray(vao);
-
-      /* Iterate through all characters */
-      for (auto c = text.begin(); c != text.end(); c++) {
-        Character ch = characters[*c];
-
-        GLfloat xpos = position.x + ch.bearing.x * scale;
-        GLfloat ypos = position.y - ch.bearing.y * scale;
-
-        GLfloat w = ch.size.x * scale;
-        GLfloat h = ch.size.y * scale;
-
-        /* Update VBO for each character */
-        GLfloat vertices[24] = {
-          xpos,     ypos + h,   0.0, 1.0,            
-          xpos,     ypos,       0.0, 0.0,
-          xpos + w, ypos,       1.0, 0.0,
-
-          xpos,     ypos + h,   0.0, 1.0,
-          xpos + w, ypos,       1.0, 0.0,
-          xpos + w, ypos + h,   1.0, 1.0,          
-        };
-
-        /* Render glyph texture over quad */
-        glBindTexture(GL_TEXTURE_2D, ch.textureID);
-
-        /* Update content of VBO memory */
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); 
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-        /* Render quad */
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-
-        position.x += (ch.advance >> 6) * scale;
-      }
-
-      shader.disuse();
-
-      glBindVertexArray(0);
-      glBindTexture(GL_TEXTURE_2D, 0);
-    }
-};
-
 int main() {
   /* Initialize GLFW */
   glfwInit();
@@ -912,6 +997,9 @@ int main() {
 
   Font font(ft, "res/Minecraftia.ttf");
 
+  LogWindow l(vec2(12, SCREEN_HEIGHT - 12), 8, font);
+  Logger::window = &l;
+
   FPSCounter fps;
   while(!glfwWindowShouldClose(window)) {
     glfwPollEvents();
@@ -940,7 +1028,7 @@ int main() {
 
     context.disuse();
 
-    font.render("Hello, world!", vec2(12, 24), vec3(1), 0.75);
+    l.render();
 
     glfwSwapBuffers(window);
   }
